2dde2cf4da9605a6e3bd363406d35475198a4830
Donipop <minjaebae@gmail.com>
2022-12-07 15:24:25 +0900

    checkBox 수정

--- src/component/Orderlist/Orderlist.js
+++ src/component/Orderlist/Orderlist.js
@@ -1,28 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from 'axios';
-import Tr,{data2} from './Tr'
-import postcss from "postcss";
+import styled from "styled-components";
 
 
 const Orderlist = () => {
   const [info, setInfo] = useState([]);
-  const [checkItems, setCheckItems] = useState([]);
+  
 
-  function User({user}){
-   return (
-     <div>
-      <td><input type='checkbox'></input></td>
-      <td>{user.id}</td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      <td>{user.phone}</td>
-      <td>{user.website}</td>
-      
-     </div>
-
-   )
-
-  }
 //더미 데이터 호출
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users')
@@ -33,10 +17,12 @@ const Orderlist = () => {
   }, []);
   const [isChecked, setIsChecked] = useState(false);
   const [checkedItems, setCheckedItems] = useState(new Set());
-  const checkHandler = ({target}) => {
+
+  const checkHandler = (target) => {
          setIsChecked(!isChecked)
          checkedItemHandler(target.parentNode, target.value, target.checked)
-         console.log(target.parentNode)
+         console.log(target.target.parentElement);
+         console.log(info[target.target.value-1].name)
 
   }
   const checkedItemHandler = (box,id, isChecked) => {
@@ -50,47 +36,51 @@ setCheckedItems(checkedItems)
 box.style.backgroundColor='#fff';
 
     }
-    return checkItems
+    return checkedItems
 
   }
   
-  const ddd = document.getElementById('ddd')
 
   return (
     <div className="container max-w-screen-lg mx-auto">
       <div className='text-xl font-bold mt-5 mb-3 text-center'>고객 정보 리스트</div>
-        <thead className=''>
-          <tr className=''>
-            <td className="">Id.</td>
-            <td className="">Name</td>
-            <td className="">Email</td>
-            <td className="">Phone No.</td>
-            <td className="">Website</td>
-          </tr>
-         <tr className="bg-white border-2 border-gray-20"> 
+        
+          
+         
          <div>
-
-          {info.map((user) => (<label key = {user.id}>
-            <td><input type="checkbox" value={user.id} onChange={(e) => checkHandler(e)}/></td>
-            <tr>
-              <td  >{user.id}</td>  
-              <td >{user.name}</td>
-              <td >{user.email}</td>
-              <td  >{user.phone}</td>
-              <td >{user.website}</td>
-            </tr>
-            
-             </label>
-            
-             
+          <tr>
+            <Td><input type="checkbox"></input></Td>
+            <Td>Id.</Td>
+            <Td>Name</Td>
+            <Td>Email</Td>
+            <Td>Phone No.</Td>
+            <Td>Website</Td>
+          </tr>
+          {info.map((user) => (
+          <tr>
+            <Td><input type="checkbox" value={user.id} onChange={(e) => checkHandler(e)}/></Td>
+            <Td>{user.id}</Td>  
+            <Td>{user.name}</Td>
+            <Td>{user.email}</Td>
+            <Td>{user.phone}</Td>
+            <Td>{user.website}</Td>
+          </tr>
           ))}
-             </div>
 
-         </tr>
-         </thead>
+          </div>
+         
+         
          
          </div>
   );
+  
 };
 
 export default Orderlist;
+
+const Td = styled.td`
+    
+      padding-left: 10px;
+      padding-right: 10px;
+    
+`
