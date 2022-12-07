d250315f443a59d055f94b9b361f0e952ba8e12b
Donipop <minjaebae@gmail.com>
2022-12-07 21:19:08 +0900

    카테고리 API 작업중

--- src/component/sellercenter/Create.js
+++ src/component/sellercenter/Create.js
@@ -1,19 +1,43 @@
 ////////////////////////////////////
 // 상품등록 페이지
 ////////////////////////////////////
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../css/create.css'
+import axios from 'axios';
 
-
-
+/**
+ * 상품등록 페이지 컴포넌트
+ * @returns create page
+ */
 function Create(){
+    const [categorylist, setCategorylist] = useState({
+        category1: [],
+        category2: [],
+        category3: [],
+        category4: []
+    });
+    
+    useEffect(() => {
+        get_CategoryList().then(res =>{
+            setCategorylist(categorylist => {
+                return {
+                    ...categorylist, category1: res
+                }
+            });
+        })
+    },[]);
+    
+
     const [category, setCategory] = useState({
         category1: '',
         category2: '',
         category3: '',
         category4: ''
     });
-
+    /**
+     * dropdown menu에서 선택한 카테고리를 state에 저장
+     * 
+     */
     const onChangeCategory = (e) => {
         // setCategory( {...category, [e.target.parentElement.id]: e.target.innerText})
         if (e.target.parentElement.id.split('category')[1] !== '1'){
@@ -22,7 +46,13 @@ function Create(){
                     ...category, [e.target.parentElement.id]: ' > ' + e.target.innerText
                 }
             })
+            
         }else{
+            categorylist.category1.forEach((category) => {
+                if (category.name === e.target.innerText){
+                    console.log(category.num);
+                }
+            })
             setCategory((category) => {
                 return {
                     ...category, [e.target.parentElement.id]: e.target.innerText
@@ -30,12 +60,7 @@ function Create(){
             })
         }
     }
-    // for(let i in category){
-    //     console.log(category[i])
-    // }
-    // const onChangeAccount = (e) => {
-    //     setAccount({...account, [e.target.name]: e.target.value,});
-    // }
+    
     return(
         <div className='w-100'>
             <div className="m-2">
@@ -49,21 +74,12 @@ function Create(){
                             <div className='collapse row' id='create-category'>
                                 <div className='col-3'>
                                     <ul className='list-group list-group-flush scrollarea' id='category1'>
-                                        <li className='list-group-item list-group-item-action' onClick={onChangeCategory}>
-                                            카테고리 대분류
-                                        </li>
-                                        <li className='list-group-item list-group-item-action'>
-                                            내용
-                                        </li>
-                                        <li className='list-group-item list-group-item-action'>
-                                            내용
-                                        </li>
-                                        <li className='list-group-item list-group-item-action'>
-                                            내용
-                                        </li>
-                                        <li className='list-group-item list-group-item-action'>
-                                            내용
-                                        </li>
+
+                                        {categorylist.category1.map((category) => (
+                                            <li key={category.num} className='list-group-item list-group-item-action' onClick={onChangeCategory}>
+                                            {category.name}
+                                        </li>))}
+
                                     </ul>
                                 </div>
 
@@ -136,5 +152,24 @@ function Create(){
             </div>
         </div>
     );
+}
+/**
+ * 
+ * @param {String} parent_num 
+ * @returns 입력한 부모를 가진 카테고리 리스트
+ */
+function get_CategoryList(parent_num){
+    const re = axios.get('http://localhost:3000/api/sellercenter/getcategory', {
+        params: {
+            parent_num: parent_num
+        }
+    })
+    return re.then((res) => {
+        console.log("카테고리 리스트 가져오기 성공");
+        // console.log(res.data);
+        return res.data;
+        }).catch((err) => {
+        return err;
+        })
 }
 export default Create;
