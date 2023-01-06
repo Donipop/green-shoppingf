34f38a0f54944e8173f00fa346881fdc9f3af1ad
OhWooSeong <p_9_0@naver.com>
2023-01-06 17:36:15 +0900

    zz

--- src/component/Header2.js
+++ src/component/Header2.js
@@ -1,35 +1,57 @@
 import './hearder2css.css'
-import React, { useRef } from 'react'
+import React, { useRef,useState } from 'react'
 import styled from 'styled-components'
 const Header2 = () => {
-    const Ref = useRef()
+    const [name, setName] = useState('전체')
+    const UlRef = useRef()
+    const ARef = useRef()
+    const ARef1 = useRef()
+    const ARef2 = useRef()
+    const ARef3 = useRef()
+    const ARef4 = useRef()
+    const ARef5 = useRef()
+    const ARef6 = useRef()
+    const ARef7 = useRef()
+    const ARef8 = useRef()
+
+
     const nonecheck = () => {
-        console.log(Ref.current.style.display)
-        if(Ref.current.style.display === "none"){
-        Ref.current.style.display = "block"
+        console.log(ARef.current.innerHTML)
+        if(UlRef.current.style.display === "none"){
+            UlRef.current.style.display = "block"
         } else {
-            Ref.current.style.display = "none"
+            UlRef.current.style.display = "none"
         }
     }
+
+    const test = (e) => {
+        setName(e.target.innerHTML)
+        UlRef.current.style.display = "none"
+    }
+
+
+   
+
     
+
 
     return (
     <div>    
         <div style={{width:"518px",border:"2px solid #4285f4", height:"37px", marginRight:"22px"}}>
                 <div style={{width:"134px",borderRight:"1px solid #ddd",height:"33px"}}>
-                    <a className="dd" onClick={nonecheck}></a>
-                    <a className="ff" onClick={nonecheck}>전체</a>
-                        <Ul ref={Ref} >
+                    <a className="dd" ></a>
+                    <a className="ff" onClick={nonecheck}>{name}</a>
+                        <Ul ref={UlRef} style={{display:"none"}} >
                             <LI><A href='#!'>전체</A></LI>
-                            <LI><A htef="#255"rel='255'>남성패션</A></LI>
-                            <LI><A>여성패션</A></LI>
-                            <LI><A>남녀공용패션</A></LI>
-                            <LI><A>유아동패션</A></LI>
-                            <LI><A>출산/유아동</A></LI>
-                            <LI><A>뷰티</A></LI>
-                            <LI><A>식품</A></LI>
-                            <LI><A>주방용퓸</A></LI>
-                            <LI><A>생활용품</A></LI>
+                            <LI><A ref={ARef} onClick={test}href="#255"rel='255'>남성패션</A></LI>
+                            <LI><A onClick={test}>여성패션</A></LI>
+                            <LI><A onClick={test}>남녀공용패션</A></LI>
+                            <LI><A onClick={test}>유아동패션</A></LI>
+                            <LI><A onClick={test}>출산/유아동</A></LI>
+                            <LI><A onClick={test}>뷰티</A></LI>
+                            <LI><A onClick={test}>식품</A></LI>
+                            <LI><A onClick={test}>주방용퓸</A></LI>
+                            <LI><A onClick={test}>생활용품</A></LI>
                             </Ul>
                 </div>
         </div>
@@ -55,7 +77,7 @@ const A = styled.a`
 const Ul = styled.ul`
     top: 35px;
     width: 138px;
-    max-height: 702px;
+    max-height: 200px;
     padding: 10px 0 10px 10px;
     border: 1px solid #ddd;
     left: -2px;
